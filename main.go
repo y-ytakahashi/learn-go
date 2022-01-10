420aa46cebab8502788adc2a5fbb94f683aba629
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2022-01-10 22:38:51 +0900

    標準ライブラリでWebサーバー

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: nil,
+	}
+
+	server.ListenAndServe()
 
 	fmt.Println("hello go")
 }
