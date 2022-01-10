1da05050b92aface9d81bec00cb70411e9147424
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2022-01-10 22:55:48 +0900

    web server で hello world

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello World")
+}
 func main() {
 
+	handler := MyHandler{}
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Handler: &handler,
 	}
 
 	server.ListenAndServe()
